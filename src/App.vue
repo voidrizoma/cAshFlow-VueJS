4cf464632ddb283bb637e512f8492d42425f6f3c
voidrizoma <milhadasdepapel@gmail.com>
2022-06-18 18:55:29 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import SplashScreen from "@/components/SplashScreen.vue";
+
+export default {
+  components: {
+   SplashScreen
+  }
+}
+</script>
+
+<template>
+  <SplashScreen/>
+</template>
+
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+
+</style>
